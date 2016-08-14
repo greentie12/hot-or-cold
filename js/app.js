91ae168305ca7ba2de1e00e44eacace4fe9bf878
greentie12 <greentie12@gmail.com>
2016-08-14 18:17:53 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+    /*--- Display information modal box ---*/
+    $(".what").click(function() {
+        $(".overlay").fadeIn(1000);
+
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function() {
+        $(".overlay").fadeOut(1000);
+    });
+
+    $("a.new").click(function(event) {
+    	event.preventDefault();
+    	count = 0;
+    	$('#feedback').html("Make your Guess!").css({ "background-color": "#cc324b", "color": "white", "font-weight": "bold" });
+    	$('#count').html(count);
+    	$('#guessList').html(" ");
+    	//$('input#userGuess.text').html(" ");
+
+    });
+
+    var compNum = Math.floor(Math.random() * 100 + 1);
+    var count = 0;
+
+    /*---Start New Game---*/
+    $("#guessButton").click(function(event) {
+        event.preventDefault();
+        count++;
+        var guess = $('#userGuess').val();
+        //var compNum = Math.floor(Math.random() * 2 + 1);
+        //var count = true;
+
+        //while (count) {
+        if (guess == compNum) {
+            //$('#guessList').append("Congratulations. You have guessed the number!");
+            $('#feedback').html("Congratulations. You have guessed the number!").css({ "background-color": "#cc324b", "color": "white", "font-weight": "bold" });
+        } else if (Math.abs(compNum - guess) <= 10 && Math.abs(compNum - guess) > 0) {
+            $('#guessList').append(guess + " ");
+            $('#feedback').html("Very Hot!").css({ "background-color": "#cc324b", "color": "white", "font-weight": "bold" });
+            $('#count').html(count);
+        } else if (Math.abs(compNum - guess) > 10 && Math.abs(compNum - guess) <= 20) {
+            $('#guessList').append(guess + " ");
+            $('#feedback').html("Hot!").css({ "background-color": "#ff8282", "color": "white", "font-weight": "bold" });
+            $('#count').html(count);
+        } else if (Math.abs(compNum - guess) > 21 && Math.abs(compNum - guess) < 30) {
+            $('#guessList').append(guess + " ");
+            $('#feedback').html("Warm").css({ "background-color": "#FFA500", "color": "black", "font-weight": "bold" });
+            $('#count').html(count);
+        } else {
+            $('#guessList').append(guess + " ");
+            $('#feedback').html("Cold").css({ "background-color": "#82CFFD", "color": "black", "font-weight": "bold" });
+            $('#count').html(count);
+        }
+        //count = false;
+    });
+});
